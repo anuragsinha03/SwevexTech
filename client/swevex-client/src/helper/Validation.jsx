51275d258967f2b9c214dc38270eb97d007ffdaa
Anurag <bhootnath@gmail.com>
2023-02-03 16:57:32 +0530

    another push

--- client/swevex-client/src/helper/Validation.jsx
+++ client/swevex-client/src/helper/Validation.jsx
@@ -1,48 +1,45 @@
-
 export async function contactFormValidate(values) {
-    const errors = contactFormVerify({}, values)
-    return errors
+  const errors = contactFormVerify({}, values);
+  return errors;
 }
 
 const contactFormVerify = (error = {}, values) => {
+  if (!values.name) {
+    error.name = "name Required";
+  } else if (values.name.length <= 3) {
+    error.name = "name should be greater than 3 characters";
+  }
 
+  if (!values.email) {
+    error.email = "email Required";
+  } else if (values.email.includes(" ")) {
+    error.email = "Invalid email";
+  } else if (values.email.length <= 3) {
+    error.email = "email should be greater than 3 characters";
+  }
 
-    if (!values.name) {
-        error.name = "name Required";
-    } else if (values.name.length < 2) {
-        error.name = "name should be greater than 2 characters"
-    }
+  if (!values.phone) {
+    error.phone = "phone Required";
+  } else if (values.phone.includes(" ")) {
+    error.phone = "Invalid phone";
+  } else if (values.phone.length < 10) {
+    error.phone = "Phone number should be 10 digit";
+  }
 
-    if (!values.email) {
-        error.email = "email Required";
-    } else if (values.email.includes(" ")) {
-        error.email = "Invalid email"
-    } else if (values.email.length < 2) {
-        error.email = "email should be greater than 2 characters"
-    }
+  if (!values.subject) {
+    error.subject = "subject Required";
+  } else if (values.subject.length <= 3) {
+    error.subject = "name should be greater than 3 characters";
+  }
 
-    if (!values.phone) {
-        error.phone = "phone Required";
-    } else if (values.phone.includes(" ")) {
-        error.phone = "Invalid phone"
-    } else if (values.phone.length < 10) {
-        error.phone = "Phone number should be 10 digit"
-    }
-
-    if (!values.subject) {
-        error.subject = "subject Required";
-    } else if (values.subject.length < 2) {
-        error.subject = "name should be greater than 2 characters"
-    }
-
-    if (!values.message) {
-        error.message = "message Required";
-    } else if (values.message.length < 2) {
-        error.message = "name should be greater than 2 characters"
-    }
-    console.log(error)
-    return error
-}
+  if (!values.message) {
+    error.message = "message Required";
+  } else if (values.message.length <= 3) {
+    error.message = "name should be greater than 3 characters";
+  }
+  console.log(error);
+  return error;
+};
 
 // export async function loginValidation(values) {
 //     const errors = registerVerify({}, values)
