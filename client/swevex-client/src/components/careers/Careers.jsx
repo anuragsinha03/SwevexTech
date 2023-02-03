ed2b527c74ea02eb3248c64bc64970a66f7c1e27
Raj Gupta <pratikrajgupta23@gmail.com>
2023-02-03 12:55:44 +0530

    added the backend

--- client/swevex-client/src/components/careers/Careers.jsx
+++ client/swevex-client/src/components/careers/Careers.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Navbar from "../home/Navbar";
 import careers from "../../images/careers.svg";
-import sales from "../../images/customer-support.svg";
-import marketing from "../../images/marketing.svg";
-import technical from "../../images/web-development.svg";
 import Footer from "../home/Footer";
+import { CarrierContainerData } from './CarrierContainerData'
+import CarrierContainerCard from "./CarrierContainerCard";
 function Careers() {
   return (
     <div>
@@ -18,40 +17,18 @@ function Careers() {
             </span>
           </div>
         </div>
-
         <div className="max-width-container my-4 flex flex-col justify-center items-center gap-4 md:flex-row ">
-          <div className="border-2 border-black flex flex-col justify-center items-center p-8 rounded hover:bg-[#0894DE] hover:text-white">
-            <img className="w-[100px] aspect-square" src={sales} alt="" />
-            <p className="text-[28px] font-medium">SALES</p>
-            <p className="text-center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.{" "}
-            </p>
-          </div>
-          <div className="border-2 border-black flex flex-col justify-center items-center p-8 rounded hover:bg-[#0894DE] hover:text-white ">
-            <img className="w-[100px] aspect-square" src={marketing} alt="" />
-            <p className="text-[28px] font-medium">MARKETING</p>
-            <p className="text-center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.{" "}
-            </p>
-          </div>
-          <div className="border-2 border-black flex flex-col justify-center items-center p-8 rounded hover:bg-[#0894DE] hover:text-white ">
-            <img className="w-[100px] aspect-square" src={technical} alt="" />
-            <p className="text-[28px] font-medium">TECHNICAL</p>
-            <p className="text-center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.{" "}
-            </p>
-          </div>
+          {CarrierContainerData.map((val, index) => {
+            return (
+              <CarrierContainerCard
+                key={index}
+                serviceImg={val.serviceImg}
+                serviceTitle={val.serviceTitle}
+                serviceDescription={val.serviceDescription}
+              />
+            )
+          })}
         </div>
-
-        <div className="bg-[#0894DE] max-width-container text-center text-white gap-8 p-4 text-[24px] rounded mb-8">
-          <a className="" href="/careers/apply">
-            APPLY HERE!
-          </a>
-        </div>
-
         <Footer />
       </section>
     </div>
