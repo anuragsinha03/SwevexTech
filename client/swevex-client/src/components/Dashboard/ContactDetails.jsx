9d8a8174fe28e4a78cd076de2dac3ead78e482f4
Anurag <bhootnath@gmail.com>
2023-02-04 19:53:41 +0530

    finished mapping contact details data from backend to frontend

--- client/swevex-client/src/components/Dashboard/ContactDetails.jsx
+++ client/swevex-client/src/components/Dashboard/ContactDetails.jsx
@@ -1,34 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { getContactDetails } from '../../api/endpointApi'
-import DashboardNavbar from './DashboardNavbar'
-import TableContent from './TableContent'
+import React, { useEffect, useState } from "react";
+import { getContactDetails } from "../../api/endpointApi";
+import DashboardNavbar from "./DashboardNavbar";
+import TableContent from "./TableContent";
 
 function ContactDetails() {
-    const [contactDetails, setContactDetails] = useState()
-    useEffect(() => {
-        getContactDetails().then(response => console.log(response));
-    })
-    return (
-        <div>
-            <div className='dasboard-container | h-[100vh] max-width-container'>
-                <DashboardNavbar />
-                <div className='main-content h-[70%] '>
-                    <div className="table-container | grid grid-flow-col mt-4">
-                        <div className="name border-2 border-black text-center">name</div>
-                        <div className="email border-2 border-black text-center">Email</div>
-                        <div className="phone-no border-2 border-black text-center">Phone Number</div>
-                        <div className="subject border-2 border-black text-center">Subject</div>
-                        <div className="message border-2 border-black text-center">Message</div>
-                        <div className="message border-2 border-black text-center">Delete</div>
-                    </div>
-                    <div className="table-contents | h-full overflow-y-auto border-[1px] border-black mt-4">
-                        <TableContent />
-                    </div>
-                </div>
+  const [contactDetails, setContactDetails] = useState([]);
+  useEffect(() => {
+    getContactDetails().then((response) => {
+      setContactDetails(response.data.Details);
+    });
+  }, []);
+  console.log(contactDetails);
 
+  return (
+    <div>
+      <div className="dasboard-container | h-[100vh] max-width-container">
+        <DashboardNavbar />
+        <div className="main-content h-[70%] ">
+          <div className="table-container | grid grid-flow-col mt-4">
+            <div className="name border-2 border-black text-center">name</div>
+            <div className="email border-2 border-black text-center">Email</div>
+            <div className="phone-no border-2 border-black text-center">
+              Phone Number
             </div>
+            <div className="subject border-2 border-black text-center">
+              Subject
+            </div>
+            <div className="message border-2 border-black text-center">
+              Message
+            </div>
+            <div className="message border-2 border-black text-center">
+              Delete
+            </div>
+          </div>
+          <div className="table-contents | h-full overflow-y-auto border-[1px] border-black mt-4">
+            {contactDetails.map((value) => {
+              return (
+                <TableContent
+                  key={value._id}
+                  name={value.name}
+                  email={value.email}
+                  phone={value.phone}
+                  subject={value.subject}
+                  message={value.message}
+                />
+              );
+            })}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ContactDetails
+export default ContactDetails;
--- client/swevex-client/src/components/Dashboard/TableContent.jsx
+++ client/swevex-client/src/components/Dashboard/TableContent.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function TableContent() {
-    return (
-        <div>
-            <div className="table-content | grid grid-flow-col  ">
-                <div className="name border-b-[1px]  border-black ">name</div>
-                <div className="email border-b-[1px] border-black ">Email</div>
-                <div className="phone-no border-b-[1px] border-black ">Phone Number</div>
-                <div className="subjecb border-b-[1px] border-black ">Subject</div>
-                <div className="message border-b-[1px] border-black ">Message</div>
-                <div className="delete-btn-container border-b-[1px] border-black pr-3 flex justify-end">
-                    <button className="delete-btn border-[1px] border-black px-2" type='button'>X</button>
-                </div>
-            </div>
+function TableContent(props) {
+  return (
+    <div>
+      <div className="table-content | grid grid-flow-col  ">
+        <div className="name border-b-[1px]  border-black ">{props.name}</div>
+        <div className="email border-b-[1px] border-black ">{props.email}</div>
+        <div className="phone-no border-b-[1px] border-black ">
+          {props.email}
         </div>
-    )
+        <div className="subjecb border-b-[1px] border-black ">
+          {props.subject}
+        </div>
+        <div className="message border-b-[1px] border-black ">
+          {props.message}
+        </div>
+        <div className="delete-btn-container border-b-[1px] border-black pr-3 flex justify-end">
+          <button
+            className="delete-btn border-[1px] border-black px-2"
+            type="button"
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TableContent
+export default TableContent;
