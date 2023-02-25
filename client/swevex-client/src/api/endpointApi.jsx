723baa03e8ae004afd1c569d9d4a7d0edb46b46c
Anurag <bhootnath@gmail.com>
2023-02-25 13:53:58 +0530

    dangerous commit 2

--- client/swevex-client/src/api/endpointApi.jsx
+++ client/swevex-client/src/api/endpointApi.jsx
@@ -1,87 +1,99 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8080/api/auth/";
-const PRIVATE_URL = "http://localhost:8080/api/private";
+const BASE_URL = "/api/auth/";
+const PRIVATE_URL = "/api/private";
 const config = {
-    Headers: {
-        "Content-type": "Application/json"
-    }
-}
+  Headers: {
+    "Content-type": "Application/json",
+  },
+};
 export async function ContactFormAPI(values) {
-    const URL = `${BASE_URL}contact-us`
-    try {
-        const response = await axios.post(URL, values, config)
-        return response
-    } catch (error) {
-        return error.message
-    }
+  const URL = `${BASE_URL}contact-us`;
+  try {
+    const response = await axios.post(URL, values, config);
+    return response;
+  } catch (error) {
+    return error.message;
+  }
 }
 
 export async function SendCarrierData(values) {
-    const URL = `${BASE_URL}carrier-apply`
-    try {
-        const response = await axios.post(URL, values, { headers: { "Content-Type": "multipart/form-data" } })
-        return response
-    } catch (error) {
-        return error
-    }
+  const URL = `${BASE_URL}carrier-apply`;
+  try {
+    const response = await axios.post(URL, values, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
 
-
 export async function handleLogin(values) {
-    try {
-        const response = await axios.post(`${PRIVATE_URL}/admin-login`, values, config);
-        return response;
-    } catch (error) {
-
-        return error.response.data.error
-    }
+  try {
+    const response = await axios.post(
+      `${PRIVATE_URL}/admin-login`,
+      values,
+      config
+    );
+    return response;
+  } catch (error) {
+    return error.response.data.error;
+  }
 }
 
 export async function handleAuthVerification(token) {
-    try {
-        const response = await axios.get(`${PRIVATE_URL}/verify-token`, { headers: { "Authorization": `Bearer ${token}` } })
-        return response
-    } catch (error) {
-        return error
-    }
+  try {
+    const response = await axios.get(`${PRIVATE_URL}/verify-token`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function getContactDetails() {
-    try {
-        const response = await axios.get(`${PRIVATE_URL}/get-contact-details`)
-        return response
-    } catch (error) {
-        return error
-    }
+  try {
+    const response = await axios.get(`${PRIVATE_URL}/get-contact-details`);
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function getCarrierApplyDetails() {
-    try {
-        const response = await axios.get(`${PRIVATE_URL}/get-carrier-apply-details`)
-        return response
-    } catch (error) {
-        return error
-    }
+  try {
+    const response = await axios.get(
+      `${PRIVATE_URL}/get-carrier-apply-details`
+    );
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function deleteCarrierApplyDetails(id) {
-    const response = await axios.delete(`${PRIVATE_URL}/delete-carrier-apply-details/${id}`)
-    return response
+  const response = await axios.delete(
+    `${PRIVATE_URL}/delete-carrier-apply-details/${id}`
+  );
+  return response;
 }
-
 
 export async function ValidateEmailAndSendOtp(emailId) {
-    const response = await axios.post(`${BASE_URL}send-otp`, { emailId }, config);
-    return response
+  const response = await axios.post(`${BASE_URL}send-otp`, { emailId }, config);
+  return response;
 }
 
-
 export async function validateOTP(otp) {
-    const response = await axios.post(`${BASE_URL}validate-otp`, { otp }, config);
-    return response
+  const response = await axios.post(`${BASE_URL}validate-otp`, { otp }, config);
+  return response;
 }
 
 export async function loginChangePassword(password) {
-    const response = await axios.put(`${BASE_URL}change-password`, { password }, config);
-    return response
+  const response = await axios.put(
+    `${BASE_URL}change-password`,
+    { password },
+    config
+  );
+  return response;
 }
